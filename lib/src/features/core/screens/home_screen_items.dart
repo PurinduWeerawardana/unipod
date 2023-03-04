7fdab1bdaa41254f7a10298974b3025e0fad9ba0
Ginura Ransika De Seram <ginuraransika@gmail.com>
2023-03-04 13:11:28 +0530

    Connected the profile page and update the build.gradle to get the user location persmission

--- lib/src/features/core/screens/home_screen_items.dart
+++ lib/src/features/core/screens/home_screen_items.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:unipod/src/features/core/screens/profile/profile_screen.dart';
 import 'map/home_page_map.dart';
 
 const homeScreenItems = [
   HomePageMap(),
-  Center(child: Text("Game")),
+  Center(child: Text("Gsgsdgame")),
   Center(child: Text("Message")),
-  Center(child: Text("Account")),
+  ProfileScreen()
 ];
