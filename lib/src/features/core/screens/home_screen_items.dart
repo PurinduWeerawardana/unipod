be42f5eda4491f8cec11c35ce9e10960e7e908d7
Ginura Ransika De Seram <ginuraransika@gmail.com>
2023-03-04 15:43:04 +0530

    added the location tracking part

--- lib/src/features/core/screens/home_screen_items.dart
+++ lib/src/features/core/screens/home_screen_items.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:unipod/src/features/core/screens/game/game_screen.dart';
 import 'package:unipod/src/features/core/screens/profile/profile_screen.dart';
 import 'map/home_page_map.dart';
 
 const homeScreenItems = [
   HomePageMap(),
-  Center(child: Text("Gsgsdgame")),
+  GameScreen(),
   Center(child: Text("Message")),
   ProfileScreen()
 ];
